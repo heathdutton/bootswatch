7422f532ee5f4ef9a4ba9f0346715fbc01088035
heathdutton <hdutton@thedmsgrp.com>
2017-09-19 12:07:32 -0400

    Update all vendor dependencies and switch to Bootswatch with SCSS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@
 
     $(document).tooltip({
         selector: "[data-toggle=tooltip]"
-    })
+    });
 
     // Focus state for append/prepend inputs
     $('.input-group').on('focus', '.form-control', function () {
